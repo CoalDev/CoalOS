4a428cbb273887abc1d8870d00831a454d558e3a
Lior <coraauragithub@gmail.com>
2021-05-01 00:15:01 +0300

    Typo
    Fixed typo, stack started at 0x9000 instead of 0x8000

--- boot/bootsector.asm
+++ boot/bootsector.asm
@@ -4,7 +4,7 @@
     mov al, 0x03
     int 0x10
 
-    mov bp, 0x9000      ; Setting the stack at 0x8000 away from the bootsector
+    mov bp, 0x8000      ; Setting the stack at 0x8000 away from the bootsector
     mov sp, bp          ; setting the stack pointer to the top of the stack which is empty
     
     call switch_pm      ; Starting the switch to protected mode
