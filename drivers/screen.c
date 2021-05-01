bf317899257d61fb246d08fd2763ba1999e9886e
Lior <coraauragithub@gmail.com>
2021-05-01 12:53:55 -0700

    Added screen driver
    Added screen driver, ability to clear screen and print every where on the screen with boundary and memory limits

---
+++ drivers/screen.c
@@ -0,0 +1,108 @@
+#include "ports.h"
+#include "screen.h"
+
+int get_curser_offset();
+void set_curser_offset(int offset);
+int print_char(char c, int col, int row, char attr);
+
+int get_offset(int col, int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+void kprint_pos(char* msg, int col, int row){
+    int offset;
+    if (col >= 0 && row >= 0){
+        offset = get_offset(col, row);
+    } else {
+        offset = get_curser_offset();
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    }
+
+    int i;
+    while(msg[i] != 0){
+        offset = print_char(msg[i++], col, row, WHITE_ON_BLACK);
+        row = get_offset_row(offset);
+        col = get_offset_col(offset);
+    }
+}
+
+void kprint(char* msg){
+    kprint_pos(msg, -1, -1);
+}
+
+int print_char(char c, int col, int row, char attr){
+    char* vid_mem = (unsigned char*) VIDEO_MEM_ADDRESS;
+    if (!attr){
+        attr = WHITE_ON_BLACK;
+    }
+
+    if(col >= MAX_COLS || row >= MAX_ROWS){
+        vid_mem[2 * MAX_ROWS * MAX_COLS - 2] = 'E';
+        vid_mem[2 * MAX_ROWS * MAX_COLS - 1] = RED_ON_BLACK;
+        return get_offset(col, row);
+    }
+
+    int offset;
+    if(col >= 0 && row >= 0){
+        offset = get_offset(col, row);
+    } else {
+        offset = get_curser_offset();
+    }
+
+    if(c == '\n'){
+        row = get_offset_row(offset);
+        offset = get_offset(0, row + 1);
+    } else {
+        vid_mem[offset] = c;
+        vid_mem[offset+1] = attr;
+        offset += 2;
+    }
+    set_curser_offset(offset);
+    return offset;
+}
+
+int get_curser_offset(){
+    int vga_offset;
+    
+    port_byte_out(CRTC_ADDR_REG, 14);
+    vga_offset = port_byte_in(CRTC_DATA_REG) << 8;
+
+    port_byte_out(CRTC_ADDR_REG, 15);
+    vga_offset += port_byte_in(CRTC_DATA_REG);
+
+    return vga_offset * 2;
+}
+
+void set_curser_offset(int offset){
+    offset /= 2;
+    port_byte_out(CRTC_ADDR_REG, 14);
+    port_byte_out(CRTC_DATA_REG, (unsigned char)(offset >> 8));
+    port_byte_out(CRTC_ADDR_REG, 15);
+    port_byte_out(CRTC_DATA_REG, (unsigned char)(offset & 0xff));
+}
+
+
+void clear_screen(){
+    int screen_size = MAX_COLS * MAX_ROWS;
+    char* vid_mem = (unsigned char*) VIDEO_MEM_ADDRESS;
+    int i;
+    for(i = 0; i < screen_size; i++){
+        vid_mem[i * 2] = ' ';
+        vid_mem[(i * 2) + 1] = WHITE_ON_BLACK;
+    }
+
+    set_curser_offset(get_offset(0, 0));
+}
+
+int get_offset(int col, int row){
+    return 2 * (row * MAX_COLS + col);
+}
+
+int get_offset_row(int offset){
+    return offset / (2 * MAX_COLS);
+}
+
+int get_offset_col(int offset){
+    return (offset - (get_offset_row(offset) * 2 * MAX_COLS))/2;
+}
---
+++ drivers/screen.h
@@ -0,0 +1,13 @@
+#define VIDEO_MEM_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define WHITE_ON_BLACK 0x0f
+#define RED_ON_BLACK 0x04
+
+#define CRTC_ADDR_REG 0x3d4
+#define CRTC_DATA_REG 0x3d5
+// https://web.stanford.edu/class/cs140/projects/pintos/specs/freevga/vga/portidx.htm
+
+void clear_screen();
+void kprint_pos(char* msg, int col, int row);
+void kprint(char* msg);
